0a459d9f3ecd506ac770b32ef6469ba86bf2b235
Ramon Schmidt <ramonroc@gmail.com>
2024-07-07 18:04:15 -0300

    add aws repositories

--- locals.tf
+++ locals.tf
@@ -14,5 +14,21 @@ locals {
             topics = ["terraform", "github"]
             visibility = "public"
         }
+
+        "poc-terraform-aws-network" = {
+            description = "Automation for AWS network"
+            gitignore_template = "Terraform"
+            name = "poc-terraform-aws-network"
+            topics = ["terraform", "github", "aws", "network"]
+            visibility = "public"
+        }
+
+        "poc-terraform-aws-cluster" = {
+            description = "Automation for AWS ECS cluster"
+            gitignore_template = "Terraform"
+            name = "poc-terraform-aws-cluster"
+            topics = ["terraform", "github", "aws", "ecs"]
+            visibility = "public"
+        }
     }
 }
